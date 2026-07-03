35e6df69eea819d110af30e20706817117c7c8b3
ゆかぽ <ban2333666@icloud.com>
2026-07-03 20:08:47 +0900

    fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Yukapo Original'
+description 'QBox craft toolbox'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/recipes.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_inventory',
+    'qbx_core'
+}
+
+ui_page 'web/index.html'
+
+files {
+    'web/index.html',
+    'web/style.css',
+    'web/app.js'
+}
