f1efddf70eb7683400c965e7fed42b314387251b
ゆかぽ <ban2333666@icloud.com>
2026-07-03 20:07:40 +0900

    shared

---
+++ shared/recipes.lua
@@ -0,0 +1,22 @@
+Recipes = {
+    {
+        label = 'ロックピック',
+        item = 'lockpick',
+        amount = 1,
+        craftTime = 5000,
+        category = 'tools',
+        ingredients = {
+            iron = 1
+        }
+    },
+    -- {
+    --     label = '包帯',
+    --     item = 'bandage',
+    --     amount = 5,
+    --     craftTime = 4000,
+    --     category = 'medical',
+    --     ingredients = {
+    --         cloth = 2
+    --     }
+    -- },
+}
